5cd742988b9464eb5745a8a005a662af830ffb5a
Sagar Shourov Roy <sagarshourov@gmail.com>
2023-04-14 20:30:18 +0600

    up

--- front/src/views/dashboard/Profile.jsx
+++ front/src/views/dashboard/Profile.jsx
@@ -552,7 +552,11 @@ const UserMain = () => {
             )}
             {/* pleaseCheckTheEnglishTestScoresIfYouHaveAny */}
 
-            <h3 className="my-5">Tasks</h3>
+            <h3 className="my-5 text-bolder">Tasks</h3>
+
+            <div className="grid">
+
+            </div>
 
             <div className="box intro-y p-5 mt-5 pb-5">
               <div className="flex items-center border-b border-slate-200/60 dark:border-darkmode-400 pb-5 mb-5">
