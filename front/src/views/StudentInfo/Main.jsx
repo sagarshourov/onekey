4acff02ea222452aa17471b2e2673d11b45a1e52
Sagar Shourov Roy <sagarshourov@gmail.com>
2023-06-08 19:16:36 +0600

    update from laptop

--- front/src/views/StudentInfo/Main.jsx
+++ front/src/views/StudentInfo/Main.jsx
@@ -207,42 +207,44 @@ const AdminUsers = (props) => {
         {/* END: Pagination */}
       </div>
       {/* BEGIN: Delete Confirmation Modal */}
-      <Modal
-        show={showStudentInfrmation}
-        onHidden={() => {
-          handelModel(false);
-        }}
-      >
-        <ModalBody className="p-0">
-          <form onSubmit={(e) => updateInformation(e)}>
-            <div className="p-5 ">
-              <h3 className="text-lg text-center py-3 mb-4">
-                Student Information
-              </h3>
-              <div>
-                <label htmlFor="vertical-form-1" className="form-label">
-                  SEVIS ID number
-                </label>
+      {fdata.user_id &&
 
-                <div className="input-group w-full">
-                  <div className="input-group-text">
-                    <Lucide icon="Code" className="w-4 h-4 mt-2" />
+        <Modal
+          show={showStudentInfrmation}
+          onHidden={() => {
+            handelModel(false);
+          }}
+        >
+          <ModalBody className="p-0">
+            <form onSubmit={(e) => updateInformation(e)}>
+              <div className="p-5 ">
+                <h3 className="text-lg text-center py-3 mb-4">
+                  Student Information
+                </h3>
+                <div>
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    SEVIS ID number
+                  </label>
+
+                  <div className="input-group w-full">
+                    <div className="input-group-text">
+                      <Lucide icon="Code" className="w-4 h-4 mt-2" />
+                    </div>
+                    <input
+                      type="text"
+                      name="code"
+                      className=" py-4 form-control"
+                      placeholder=""
+                      defaultValue={fdata.code}
+                    />
                   </div>
-                  <input
-                    type="text"
-                    name="code"
-                    className=" py-4 form-control"
-                    placeholder=""
-                    defaultValue={fdata.code}
-                  />
                 </div>
-              </div>
-              <div className="mt-5">
-                <label htmlFor="vertical-form-1" className="form-label">
-                  Case Type
-                </label>
+                <div className="mt-5">
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    Case Type
+                  </label>
 
-                <div className="input-group w-full">
+                  {/* <div className="input-group w-full">
                   <div className="input-group-text">
                     <Lucide icon="Award" className="w-4 h-4 mt-2" />
                   </div>
@@ -260,148 +262,177 @@ const AdminUsers = (props) => {
                       <option value="4">Silver Package </option>
                     </select>
                   )}
-                </div>
-              </div>
-              <div className="mt-5">
-                <label htmlFor="vertical-form-1" className="form-label">
-                  Interview Date
-                </label>
+                </div> */}
 
-                <div className="input-group w-full">
-                  <div className="input-group-text">
-                    <Lucide icon="Calendar" className="w-4 h-4 mt-2" />
+
+                  <div className="input-group w-full">
+                    <div className="input-group-text">
+                      <Lucide icon="Award" className="w-4 h-4 mt-2" />
+                    </div>
+                    {fdata.package !== 0 ? (
+                      <select
+                        name="package"
+                        defaultValue={fdata.package}
+                        className=" py-4 form-control"
+                      >
+                        <option>Select ... </option>
+                        <option value="1">Other Package </option>
+                        <option value="2">Platinum Package </option>
+                        <option value="3">Gold Package </option>
+                        <option value="4">Silver Package </option>
+                      </select>
+                    ) : (
+                      <select name="package" className=" py-4 form-control">
+                        <option>Select ... </option>
+                        <option value="1">Other Package </option>
+                        <option value="2">Platinum Package </option>
+                        <option value="3">Gold Package </option>
+                        <option value="4">Silver Package </option>
+                      </select>
+                    )}
                   </div>
-                  <input
-                    type="date"
-                    name="interview_date"
-                    className=" py-4 form-control"
-                    placeholder=""
-                    defaultValue={fdata.interview_date}
-                  />
                 </div>
-              </div>
-              <div className="mt-5">
-                <label htmlFor="vertical-form-1" className="form-label">
-                  Interview Time
-                </label>
+                <div className="mt-5">
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    Interview Date
+                  </label>
 
-                <div className="input-group w-full">
-                  <div className="input-group-text">
-                    <Lucide icon="Clock" className="w-4 h-4 mt-2" />
+                  <div className="input-group w-full">
+                    <div className="input-group-text">
+                      <Lucide icon="Calendar" className="w-4 h-4 mt-2" />
+                    </div>
+                    <input
+                      type="date"
+                      name="interview_date"
+                      className=" py-4 form-control"
+                      placeholder=""
+                      defaultValue={fdata.interview_date}
+                    />
                   </div>
-                  <input
-                    type="time"
-                    name="interview_time"
-                    className=" py-4 form-control"
-                    placeholder=""
-                    defaultValue={fdata.interview_time}
-                  />
                 </div>
-              </div>
-              <div className="mt-5">
-                <label htmlFor="vertical-form-1" className="form-label">
-                  U.S. Consulate
-                </label>
+                <div className="mt-5">
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    Interview Time
+                  </label>
 
-                <div className="input-group w-full">
-                  <div className="input-group-text">
-                    <Lucide icon="UserCheck" className="w-5 h-4 mt-2" />
+                  <div className="input-group w-full">
+                    <div className="input-group-text">
+                      <Lucide icon="Clock" className="w-4 h-4 mt-2" />
+                    </div>
+                    <input
+                      type="time"
+                      name="interview_time"
+                      className=" py-4 form-control"
+                      placeholder=""
+                      defaultValue={fdata.interview_time}
+                    />
                   </div>
-
-                  <input
-                    type="text"
-                    name="us_consultant"
-                    className=" py-4 form-control"
-                    defaultValue={fdata.us_consultant}
-                  />
                 </div>
-              </div>
-              <div className="mt-5">
-                <label htmlFor="vertical-form-1" className="form-label">
-                  University / Institution
-                </label>
+                <div className="mt-5">
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    U.S. Consulate
+                  </label>
 
-                <div className="input-group w-full">
-                  <div id="input-group-email" className="input-group-text">
-                    <Lucide icon="Home" className="w-4 h-4 mt-2" />
+                  <div className="input-group w-full">
+                    <div className="input-group-text">
+                      <Lucide icon="UserCheck" className="w-5 h-4 mt-2" />
+                    </div>
+
+                    <input
+                      type="text"
+                      name="us_consultant"
+                      className=" py-4 form-control"
+                      defaultValue={fdata.us_consultant}
+                    />
                   </div>
-
-                  <input
-                    type="text"
-                    name="university"
-                    className=" py-4 form-control"
-                    defaultValue={fdata.university}
-                  />
                 </div>
-              </div>
-              <div className="mt-5">
-                <label htmlFor="vertical-form-1" className="form-label">
-                  Visa Type Status
-                </label>
+                <div className="mt-5">
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    University / Institution
+                  </label>
 
-                <div className="input-group w-full">
-                  <div className="input-group-text">
-                    <Lucide icon="Info" className="w-5 h-4 mt-2" />
+                  <div className="input-group w-full">
+                    <div id="input-group-email" className="input-group-text">
+                      <Lucide icon="Home" className="w-4 h-4 mt-2" />
+                    </div>
+
+                    <input
+                      type="text"
+                      name="university"
+                      className=" py-4 form-control"
+                      defaultValue={fdata.university}
+                    />
                   </div>
-
-                  <input
-                    type="text"
-                    name="visa_type"
-                    className=" py-4 form-control"
-                    defaultValue={fdata.visa_type}
-                  />
                 </div>
-              </div>
+                <div className="mt-5">
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    Visa Type Status
+                  </label>
 
-              <div className="mt-5">
-                <label htmlFor="vertical-form-1" className="form-label">
-                  DS-160 Number
-                </label>
+                  <div className="input-group w-full">
+                    <div className="input-group-text">
+                      <Lucide icon="Info" className="w-5 h-4 mt-2" />
+                    </div>
 
-                <div className="input-group w-full">
-                  <div className="input-group-text">
-                    <Lucide icon="GitPullRequest" className="w-5 h-4 mt-2" />
+                    <input
+                      type="text"
+                      name="visa_type"
+                      className=" py-4 form-control"
+                      defaultValue={fdata.visa_type}
+                    />
                   </div>
+                </div>
 
-                  <input
-                    type="text"
-                    name="ds_160_num"
-                    className=" py-4 form-control"
-                    defaultValue={fdata.ds_160_num}
-                  />
+                <div className="mt-5">
+                  <label htmlFor="vertical-form-1" className="form-label">
+                    DS-160 Number
+                  </label>
+
+                  <div className="input-group w-full">
+                    <div className="input-group-text">
+                      <Lucide icon="GitPullRequest" className="w-5 h-4 mt-2" />
+                    </div>
+
+                    <input
+                      type="text"
+                      name="ds_160_num"
+                      className=" py-4 form-control"
+                      defaultValue={fdata.ds_160_num}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="px-5 pb-8 text-center">
-              <button
-                type="button"
-                onClick={() => {
-                  handelModel(false);
-                }}
-                className="btn btn-outline-secondary w-24 mr-1"
-              >
-                Cancel
-              </button>
-              <button type="submit" className="btn btn-primary w-24">
-                Update{" "}
-                {loading && (
-                  <LoadingIcon
-                    icon="three-dots"
-                    color="white"
-                    className="w-4 h-4 ml-2"
-                  />
-                )}
-              </button>
+              <div className="px-5 pb-8 text-center">
+                <button
+                  type="button"
+                  onClick={() => {
+                    handelModel(false);
+                  }}
+                  className="btn btn-outline-secondary w-24 mr-1"
+                >
+                  Cancel
+                </button>
+                <button type="submit" className="btn btn-primary w-24">
+                  Update{" "}
+                  {loading && (
+                    <LoadingIcon
+                      icon="three-dots"
+                      color="white"
+                      className="w-4 h-4 ml-2"
+                    />
+                  )}
+                </button>
 
-              <input
-                type="hidden"
-                name="user_id"
-                defaultValue={fdata.user_id}
-              />
-            </div>
-          </form>
-        </ModalBody>
-      </Modal>
+                <input
+                  type="hidden"
+                  name="user_id"
+                  defaultValue={fdata.user_id}
+                />
+              </div>
+            </form>
+          </ModalBody>
+        </Modal>
+      }
       {/* END: Delete Confirmation Modal */}
     </>
   );
